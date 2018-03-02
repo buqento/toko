916b331b94879a7ffb8762576ebf7c318f775db6
buqento <buqento@gmail.com>
2018-03-02 15:16:12 +0900

    update home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, ToastController } from 'ionic-angular';
+import { AuthService } from '../../providers/auth-service/auth-service';
+import { Platform } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -7,10 +9,57 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'home.html',
 })
 export class HomePage {
+  pushPage: any;
+  produk: string = "populer";
+  isAndroid: boolean = false;
+  userDetails: any;
+  kodeBelanja: any;
+  responseData: any;
+  dataSet: any;
+  dataSetPenyedia: any;
+  productPostData = {"kategori":"", "id":"", "kodeBelanja":"", "id_user":"", "penyedia_id":""}
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(platform: Platform, 
+    public toastCtrl: ToastController,
+    public loadingCtrl: LoadingController, 
+    public navCtrl: NavController, 
+    public authService: AuthService){
+
+    this.isAndroid = platform.is('android');    
+    const data = JSON.parse(localStorage.getItem('userData'));
+    this.userDetails = data.userData;
+    const dataKodeBelanja = JSON.parse(localStorage.getItem('kodeBelanja'));
+    this.kodeBelanja = dataKodeBelanja.kodeBelanja;
+    this.productPostData.kodeBelanja = this.kodeBelanja.kode;
+    this.productPostData.id_user = this.userDetails.id;
+    
+    this.getProduct();
+    this.getPenyedia();
   }
 
-  ionViewDidLoad() {}
+  doRefresh(refresher){
+    this.getProduct();
+    refresher.complete();
+  }
+
+  getProduct(){
+    let loading = this.loadingCtrl.create({
+      spinner: 'crescent',
+      showBackdrop: true
+    })
+    loading.present();
+    this.authService.postData(this.productPostData,'getNewProduct').then((result) => {
+      this.responseData = result;
+      this.dataSet = this.responseData.newproductData;
+      loading.dismiss();
+    });
+  }
+
+  getPenyedia(){
+    this.authService.postData(this.productPostData,'penyedia').then((result) => {
+      this.responseData = result;
+      this.dataSetPenyedia = this.responseData.penyediaData;
+    });
+  }
 
 }
